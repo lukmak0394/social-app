07a0e449fb595e64b393aa475beb4afab74e45ef
lukmak0394 <lukmak0394@wp.pl>
2022-01-19 20:00:18 +0100

    created sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import './css/Form.css';
 
 function SignUp() {
+
+    function validate(event) {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h2>SignUp Here</h2>
+        <div className="wrapper">
+            <h3>Let's Join Us!</h3>
+            <form className="SignUp" onSubmit={validate}>
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="E-mail" />
+                <input type="text" placeholder="Password" />
+                <input type="text" placeholder="Repeat Password" /> 
+                <button type="submit">Sign Up</button>
+            </form>
         </div>
+       
     );
 }
 
